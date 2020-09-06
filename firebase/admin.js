5d3850a121008633b24adac5e15b8c6710dd93c0
ferca84 <fernandocaino84@gmail.com>
2020-09-06 17:36:10 -0300

    borre admin

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,12 +1,12 @@
 const admin = require("firebase-admin")
 
-const serviceAccount = require("./firebase-keys.json")
+//const serviceAccount = require("./firebase-keys.json")
 
 try {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://travelblogmap-15db6.firebaseio.com",
-  })
+  //admin.initializeApp({
+  //  credential: admin.credential.cert(serviceAccount),
+  //  databaseURL: "https://travelblogmap-15db6.firebaseio.com",
+  //})
 } catch (e) {}
 
 export const firestore = admin.firestore()
